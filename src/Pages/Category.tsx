f9e023d022f9dd72e9780ec19330fb2039f9f7bc
Anil Prajapati <anilp1291@gmail.com>
2023-01-05 16:04:42 +0530

    Update Category.tsx

--- src/Pages/Category.tsx
+++ src/Pages/Category.tsx
@@ -31,7 +31,6 @@ export function CategoryPage({ navigation }: Props) {
                 <FlatList
                     style={{ flex: 1 }}
                     data={list}
-                    numColumns={Size.isTablet?2:1}
                     ref={flatListRef}
                     renderItem={({ item, index }) => <CategoryItem key={item.id} item={item} index={index} />}
                 />
